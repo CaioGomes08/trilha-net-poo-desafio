246d2855bcb74ae9203b8e5ae41938d98e214cec
Caio Vinicius Gomes <caiovini08@outlook.com>
2024-08-07 14:05:07 -0300

    Projeto finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Celular Nokia");
+Nokia nokia = new Nokia("1133442233", "GTW12","112334", 120);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+
+Console.WriteLine("Celular Iphone");
+Iphone iphone = new Iphone("119332442", "15 pro max", "222222", 500);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
